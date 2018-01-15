835362c629dfd0e6cce83acf5acbda4fdcd160ac
RambleRainbow <three_rainbow@msn.com>
2018-01-15 13:02:28 +0800

    服务重命名

---
+++ services/cityService.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function CityService() {
+}
+
+CityService.prototype.trans = function(cityId, cityGroup) {
+}
+
+CityService.prototype.CITYGROUP_HAODAI = 10;
+CityService.prototype.CITYGROUP_RONGZI = 20;
+CityService.prototype.CITYGROUP_NIWODAI = 30;
+
+
+module.exports = new CityService();

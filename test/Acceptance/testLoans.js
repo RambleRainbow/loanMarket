34a37548fb6d33079524084262bc1b802c966aaf
RambleRainbow <three_rainbow@msn.com>
2018-01-18 16:20:45 +0800

    好贷发送逻辑测试

--- test/Acceptance/testLoans.js
+++ test/Acceptance/testLoans.js
@@ -4,9 +4,12 @@ let sinon = require('sinon');
 let _ = require('lodash');
 
 let loans = require('../../domain/loans');
-let nwd = require('../../services/niwodaiService');
 let db = require('../../services/dbService');
 let dicts = require('../../domain/dicts.js');
+let cities = require('../../domain/cities');
+let nwd = require('../../services/niwodaiService');
+let hd = require('../../services/haodaiService');
+let rz = require('../../services/rongziService');
 
 
 function objEqualto(objPart, objAll) {
@@ -111,8 +114,12 @@ describe('提交申请', function () {
 });
 
 describe('当提交申请时，应该根据规则发送向指定的网站', () => {
-  let expDoLoan;
+  //let expNwdDoLoan;
+  //let expRzDoLoan;
+  //let expHdDoLoan;
   let nwdMock;
+  let hdMock;
+  let rzMock;
   before(() => {
     db.startup();
 
@@ -120,7 +127,11 @@ describe('当提交申请时，应该根据规则发送向指定的网站', () =
 
   beforeEach(() => {
     nwdMock = sinon.mock(nwd);
-    expDoLoan = sinon.stub(nwd, 'doLoan').resolves({errorCode: 0, msg: '1你我贷测试代理：成功'});
+    hdMock = sinon.mock(hd);
+    rzMock = sinon.mock(rz);
+    //expNwdDoLoan = nwdMock.expects('doLoan').resolves({errorCode: 0, msg: '1你我贷测试代理：成功'});
+    //expRzDoLoan = rzMock.expects('doLoan').resolves({errorCode: 0, msg: '融资网测试代理：成功'});
+    //expHdDoLoan = hdMock.expects('doLoan').resolves({errorCode: 0, msg: '好贷网测试代理：成功'});
   });
 
   describe('你我贷发送逻辑', () => {
@@ -132,10 +143,12 @@ describe('当提交申请时，应该根据规则发送向指定的网站', () =
         gender: 1,
         amount: 5
       };
+      let exp = nwdMock.expects('doLoan').resolves({errorCode: 0, msg: '测试成功'});
+
       let rtn = await loans.create(testData);
 
-      expDoLoan.calledOnce.should.equal(true);
-      objEqualto(testData, expDoLoan.getCall(0).args[0]);
+      exp.calledOnce.should.equal(true);
+      objEqualto(testData, exp.getCall(0).args[0]);
     });
 
     it('当额度大于5W或者你我贷不支持的城市，则不你我贷发送', async function () {
@@ -161,21 +174,65 @@ describe('当提交申请时，应该根据规则发送向指定的网站', () =
           amount: 6
         }];
 
+      let exp = nwdMock.expects('doLoan').resolves({errorCode: 0, msg: '测试成功'});
+
       await Promise.all(_.map(testDatas, (it) => {
         return (async () => {
           await loans.create(it);
         })();
       }));
 
-      expDoLoan.callCount.should.equal(0);
+      exp.callCount.should.equal(0);
     });
   });
 
+  describe('好贷发送逻辑', () => {
+    it('贷款额度>=3W，并且符合城市', async() => {
+      await cities.init();
+      //cityId, phone, name, gender, amount
+      var testData = [
+        [ '110000','13916900000','张三',1, 3, true],
+        [ '110000','13916900000','张三',1, 4, true],
+        [ '110000x','13916900000','张三',1, 3, false],
+        [ '110000x','13916900000','张三',1, 2, false],
+
+         [ '131000','13916900000','张三',1, 3, true],
+         [ '131000','13916900000','张三',1, 4, true],
+         [ '131000x','13916900000','张三',1, 3, false],
+        [ '131000x','13916900000','张三',1, 2, false]
+      ];
+
+      hdMock.restore();
+      for(let i = 0; i <testData.length; i++) {
+        hdMock = sinon.mock(hd);
+        let exp = hdMock.expects('doLoan').resolves({errorCode: 0, msg: '好贷测试逻辑'});
+        let data = {
+          cityId: testData[i][0],
+          phone: testData[i][1],
+          name: testData[i][2],
+          gender: testData[i][3],
+          amount: testData[i][4]
+        };
+
+        let rtn = await loans.create(data);
+
+        exp.calledOnce.should.equal(testData[i][5]);
+        hdMock.restore();
+      }
+    });
+  });
 
   afterEach(() => {
     nwdMock.restore();
-    expDoLoan.restore();
-    expDoLoan.restore();
+    hdMock.restore();
+    rzMock.restore();
+    //expNwdDoLoan.restore();
+    //expRzDoLoan.restore();
+    //expHdDoLoan.restore();
+  });
+
+  describe('好贷网：额度<=3W,城市', function() {
+
   });
 
   after(() => {
